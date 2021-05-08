b137e872abddfc53508baa7555cd1cfab7b125f6
Sergey Polovko <sergey@polovko.me>
2021-05-08 17:29:43 +0300

    [os-vm] add link to a source code

--- operating-systems/os-vm/src/main/java/org/jamel/nstu/AboutDialog.java
+++ operating-systems/os-vm/src/main/java/org/jamel/nstu/AboutDialog.java
@@ -1,9 +1,14 @@
 package org.jamel.nstu;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.net.URI;
+import java.util.Objects;
 
 import javax.swing.*;
 
@@ -11,6 +16,7 @@ public class AboutDialog extends JDialog {
     private JPanel contentPane;
     private JButton buttonOK;
     private JLabel image;
+    private JLabel sourceCodeLink;
 
     public AboutDialog() {
         setContentPane(contentPane);
@@ -20,15 +26,28 @@ public class AboutDialog extends JDialog {
 
         addWindowListener(new WindowAdapter() {
             public void windowActivated(WindowEvent e) {
-                image.setIcon(new ImageIcon(getClass().getResource("/resources/images/me.gif")));
+                image.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/me.gif"))));
                 pack();
             }
         });
 
-
         buttonOK.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 onOK();
+            }
+        });
+
+        sourceCodeLink.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        sourceCodeLink.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                try {
+                    String url = ((JLabel) e.getSource()).getText();
+                    Desktop.getDesktop().browse(new URI(url));
+                } catch (Exception ex) {
+                    Thread thread = Thread.currentThread();
+                    thread.getUncaughtExceptionHandler().uncaughtException(thread, ex);
+                }
             }
         });
     }
